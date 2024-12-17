b3d3cb2214263617faac45ec77a0c51271e02a09
AlirezaAB <alirezaabtech@gmail.com>
2024-12-18 01:02:59 +0330

    Add methods to Student class: init, add_grades, calculate_average

--- student.py
+++ student.py
@@ -1,4 +1,15 @@
 from person import Person
 
 class Student(Person):
-        pass
+    def __init__(self,name , age , id ,educational_Level):
+        super().__init__(name)
+        super().__init__(age)
+        super().__init__(id)
+        self.educational_Level = educational_Level
+        self.grades = []
+
+    def add_grades(self,grade):
+        self.grades.append(grade) if 20<= grade <=0 else    print("Invalid grade! The grade must be between 0 and 20.")
+
+    def calculate_average(self):
+        return sum(self.grades) / len(self.grades) if self.grades   else    "There is not any grades!!"
